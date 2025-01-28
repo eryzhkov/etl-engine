f824202e446d69192b3ce8fb665c9aeae0a55140
Evgeny Ryzhkov <eryzhkov22@yandex.ru>
2025-01-28 16:28:10 +0300

    #22 [EDS] Unify the Apache Kafka configuration with the EMS

--- extract-data-service/src/main/java/etl/engine/extract/config/KafkaConfiguration.java
+++ extract-data-service/src/main/java/etl/engine/extract/config/KafkaConfiguration.java
@@ -1,19 +1,51 @@
 package etl.engine.extract.config;
 
+import etl.engine.extract.service.messaging.CustomJsonSerializer;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfiguration {
+
+    @Value("${eds.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Value("${eds.topics.heartbeat}")
     private String heartBeatTopicName;
 
     @Bean
+    public Map<String, Object> producerConfiguration() {
+        Map<String, Object> configuration = new HashMap<>();
+        configuration.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configuration.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configuration.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CustomJsonSerializer.class);
+        return configuration;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfiguration());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
     public NewTopic heartBeatTopic() {
+        //TODO What about retention policy?
         return TopicBuilder
                 .name(heartBeatTopicName)
                 .partitions(1)
