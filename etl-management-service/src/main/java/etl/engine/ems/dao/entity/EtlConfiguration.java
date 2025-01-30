e3e0d6c8c62912ea34de1af8f455713db80b566a
Evgeny Ryzhkov <eryzhkov22@yandex.ru>
2025-01-30 18:26:43 +0300

    #45 [EMS] EMS provides CRUD-operations for ETL-configurations

---
+++ etl-management-service/src/main/java/etl/engine/ems/dao/entity/EtlConfiguration.java
@@ -0,0 +1,77 @@
+package etl.engine.ems.dao.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "etl_configurations")
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class EtlConfiguration extends AuditableEntity implements Serializable {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "mime_type")
+    private String mimeType;
+
+    @Column(name = "spec")
+    private String specification;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "ref_etl_process_id")
+    private EtlProcess etlProcess;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "ref_etl_phase_id")
+    private EtlPhase etlPhase;
+
+    public EtlConfiguration() {
+        super();
+        this.id = UUID.randomUUID();
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Class<?> oEffectiveClass =
+                o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer()
+                        .getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass =
+                this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer()
+                        .getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) {
+            return false;
+        }
+        EtlConfiguration that = (EtlConfiguration) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass()
+                .hashCode()
+                : getClass().hashCode();
+    }
+}
